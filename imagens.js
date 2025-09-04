7dbf6c3c3e076e1d1c3d882e56a029c5eb64b7fc
CuzCuzSeco <deyvidzeusjr@gmail.com>
2025-09-04 09:05:24 -0300

    Add files via upload

---
+++ imagens.js
@@ -0,0 +1,8 @@
+[
+  { "url": "https://i.imgur.com/A7GvLfV.png", "chunk": [757, 1162], "coords": [79, 232] },
+  { "url": "https://i.imgur.com/Hwv4K41.png", "chunk": [763, 1179], "coords": [57, 249] },
+  { "url": "https://i.imgur.com/aGKmeNy.png", "chunk": [762, 1165], "coords": [105, 521] },
+  { "url": "https://i.imgur.com/jd7MyPS.png", "chunk": [1746, 801], "coords": [806, 534] },
+  { "url": "https://i.imgur.com/2I4j2WE.png", "chunk": [1814, 814], "coords": [905, 244] },
+  { "url": "https://i.imgur.com/xrJEGhe.png", "chunk": [756, 1161], "coords": [126, 14] }
+]
